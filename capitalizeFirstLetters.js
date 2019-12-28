61bfb89879978c93a23a9e6040f95fd8284ed203
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-12-28 18:32:10 +0100

    writing tests

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+
+// capitalizeFirst.js
+const assert = require('assert');
+
+// WRITE THE ACTUAL FUNCTION HERE
+
+// Check that capitalizeFirst is a function
+assert.strictEqual(typeof capitalizeFirst, 'function');
+// Check that capitalizeFirst accepts one argument
+assert.strictEqual(capitalizeFirst.length, 1);
+// Check that capitalizeFirst transforms javaScript is gold correctly
+assert.strictEqual(capitalizeFirst('javaScript is gold'), 'JavaScript Is Gold');
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirst('z'), 'Z');
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirst(''), '');
